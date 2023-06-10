0cfdd2ef8479a3bbac8b388f78fb87bc87edacca
MaazEhmed <107582675+MaazEhmed@users.noreply.github.com>
2023-06-10 13:50:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -45,6 +45,11 @@ def main():
 
 
 
+@app.route('/welcome', methods=["GET"])
+def welcome():
+    return render_template("welcome.html")
+
+
 if __name__ == "__main__":
         app.run(debug=True)
 
